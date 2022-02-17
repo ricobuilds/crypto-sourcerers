f4c4efde5ff7c1b3c4098c68e71f3d70c7a8658f
0xreeko <enric_trillo@outlook.com>
2022-02-17 11:16:34 +0000

    added blob + animations to them on Home section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,8 @@ const HomePage = () => {
   return (
     <>
       <div className="home relative container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div className="absolute right-8 bottom-9 w-64 h-64 bg-[#ed194a] rounded-full blur-3xl "></div>
+        <div className="absolute right-8 bottom-9 w-64 h-64 bg-[#ed194a] rounded-full blur-3xl opacity-70 animate-blob"></div>
+        <div className="absolute right-56 top-24 w-64 h-64 bg-[#6387f1] rounded-full blur-3xl opacity-70 animate-inverseBlob"></div>
         <div className="text space-y-6">
           <h1 className="text-6xl xl:text-8xl">
             Transferring your skills to the cryptosphere?
@@ -19,8 +20,8 @@ const HomePage = () => {
             />
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 border">
-          <p>IMG Tap into the new paradigm today. IMG</p>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 border bg-black/30 backdrop-blur-lg h-[300px] flex justify-center items-center shadow-xl">
+          <p>IMG</p>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob: "blob 7s infinite",
+        inverseBlob: "inverseBlob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "scale(1)"
+          },
+          "33%": {
+            transform: "scale(1.2)"
+          },
+          "66%": {
+            transform: "scale(0.9)"
+          },
+          "100%": {
+            transform: "scale(1)"
+          },
+        },
+        inverseBlob: {
+          "0%": {
+            transform: "scale(1)"
+          },
+          "33%": {
+            transform: "scale(0.9)"
+          },
+          "66%": {
+            transform: "scale(1.2)"
+          },
+          "100%": {
+            transform: "scale(1)"
+          },
+        }
+      }
+    },
     fontFamily: {
       'lato': ['Lato', 'sans-serif'],
     },
