e2da281c8540234dbc1c2104cae7346a274ffd7e
0xreeko <enric_trillo@outlook.com>
2022-02-19 12:35:36 +0000

    changedthe pricing cards for tailblock ones

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import PricingCard from "../components/PricingCard";
+// import PricingCard from "../components/PricingCard";
 import AudienceCard from "../components/AudienceCard";
 
 const HomePage = () => {
@@ -403,8 +403,152 @@ const HomePage = () => {
         </h1>
         <div className="pricingInfo flex flex-wrap justify-evenly items-center w-full gap-x-6 gap-y-4">
           <div className="cards gap-x-12 gap-y-4 flex flex-wrap justify-center sm:justify-start">
-            <PricingCard title={"Live Roles"} price={250} />
-            <PricingCard title={"Bounties"} price={250} />
+            <div className="w-72 h-[420px] p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
+              <h2 className="text-sm tracking-widest title-font mb-1 font-medium">
+                ROLES
+              </h2>
+              <h1 className="text-5xl text-gray-900 dark:text-gray-200 pb-4 mb-4 border-b border-gray-200 leading-none">
+                $250
+              </h1>
+              <p className="flex items-center text-gray-600 mb-2">
+                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2.5"
+                    className="w-3 h-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span>
+                Vexillologist pitchfork
+              </p>
+              <p className="flex items-center text-gray-600 mb-2">
+                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2.5"
+                    className="w-3 h-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span>
+                Tumeric plaid portland
+              </p>
+              <p className="flex items-center text-gray-600 mb-6">
+                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2.5"
+                    className="w-3 h-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span>
+                Mixtape chillwave tumeric
+              </p>
+              <button className="flex items-center mt-auto text-white bg-[#ed194a]/40 border-0 py-2 px-4 w-full focus:outline-none hover:bg-[#ed194a] rounded">
+                Button
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-4 h-4 ml-auto"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </button>
+              <p className="text-xs text-gray-500 mt-3">
+                Literally you probably haven't heard of them jean shorts.
+              </p>
+            </div>
+            <div className="w-72 h-[420px] p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
+              <h2 className="text-sm tracking-widest title-font mb-1 font-medium">
+                BOUNTIES
+              </h2>
+              <h1 className="text-5xl text-gray-900 dark:text-gray-200 pb-4 mb-4 border-b border-gray-200 leading-none">
+                $99
+              </h1>
+              <p className="flex items-center text-gray-600 mb-2">
+                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2.5"
+                    className="w-3 h-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span>
+                Vexillologist pitchfork
+              </p>
+              <p className="flex items-center text-gray-600 mb-2">
+                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2.5"
+                    className="w-3 h-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span>
+                Tumeric plaid portland
+              </p>
+              <p className="flex items-center text-gray-600 mb-6">
+                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2.5"
+                    className="w-3 h-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span>
+                Mixtape chillwave tumeric
+              </p>
+              <button className="flex items-center mt-auto text-white bg-[#ed194a]/60 border-0 py-2 px-4 w-full focus:outline-none hover:bg-[#ed194a] rounded">
+                Button
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="w-4 h-4 ml-auto"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </button>
+              <p className="text-xs text-gray-500 mt-3">
+                Literally you probably haven't heard of them jean shorts.
+              </p>
+            </div>
           </div>
           <div className="max-w-xs w-full h-[300px] border">img</div>
         </div>
