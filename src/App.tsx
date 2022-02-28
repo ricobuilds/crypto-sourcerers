0555cad8074b7bc857bba96ec41610b8179a991d
0xreeko <enric_trillo@outlook.com>
2022-02-28 18:47:13 +0000

    pseudo logos

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import Bounties from "./pages/Bounties";
 import Learning from "./components/Learning";
 import Donations from "./pages/Donations";
 import MyDropdown from "./components/DDM";
+import Logo from "./images/CSLogo3.png";
 
 const App = () => {
   const theme = localStorage.getItem("isDarkMode");
@@ -68,7 +69,8 @@ const App = () => {
           </Helmet>
           <header className=" flex justify-between items-center h-12 w-full py-3 px-6 md:px-12 lg:px-32 mb-6 border-b border-gray-200 dark:border-gray-800">
             <Link to={"/"}>
-              <div className="logo">Crypto Sourcerers</div>
+              {/* <div className="logo">Crypto Sourcerers</div> */}
+              <img src={Logo} alt="logo" className="h-9" />
             </Link>
             <div className="navs space-x-6 lg:space-x-12 font-medium hidden md:inline-block">
               <a
