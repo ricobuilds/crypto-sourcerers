166d727c3664e2f5e0d8d4a67b8e423c4da4efc5
0xreeko <enric_trillo@outlook.com>
2022-03-03 12:12:07 +0000

    added discountCode section

--- src/pages/PostARole.tsx
+++ src/pages/PostARole.tsx
@@ -67,6 +67,12 @@ const PostARole = () => {
             placeholder="Apply url/email"
             className="p-1 pl-3 outline-none rounded-md bg-black/20 focus:bg-black/60 transition ring-2 ring-gray-800 focus:ring-gray-600 tracking-widest w-full"
           />
+          <p>Discount Code</p>
+          <input
+            type="text"
+            placeholder="Enter code"
+            className="p-1 pl-3 outline-none rounded-md bg-black/20 focus:bg-black/60 transition ring-2 ring-gray-800 focus:ring-gray-600 tracking-widest w-full"
+          />
         </div>
       </div>
     </>
