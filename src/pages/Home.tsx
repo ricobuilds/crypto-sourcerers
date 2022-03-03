6622b0b750be7916904768ff0e63581e5ba7d630
0xreeko <enric_trillo@outlook.com>
2022-03-03 12:07:27 +0000

    title and description

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -335,7 +335,7 @@ const HomePage = () => {
         </p>
         <div className="pricingInfo flex flex-wrap justify-center items-center w-full gap-x-12 gap-y-4">
           <div className="cards gap-x-12 gap-y-4 flex flex-wrap justify-center sm:justify-start">
-            <div className="w-72 h-[420px] p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
+            <div className="w-72 h-[500px] p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
               <h2 className="text-sm tracking-widest title-font mb-1 font-medium">
                 VACANCY POSTS
               </h2>
@@ -377,6 +377,22 @@ const HomePage = () => {
                     <path d="M20 6L9 17l-5-5"></path>
                   </svg>
                 </span>
+                Posts will be live for 30 days
+              </p>
+              <p className="flex items-center text-gray-600 mb-2">
+                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2.5"
+                    className="w-3 h-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span>
                 Access a global pool of roleseekers
               </p>
               <p className="flex items-center text-gray-600 mb-2">
@@ -393,7 +409,7 @@ const HomePage = () => {
                     <path d="M20 6L9 17l-5-5"></path>
                   </svg>
                 </span>
-                Source talent pronto
+                Get featured in our newsletter
               </p>
               <p className="flex items-center text-gray-600 mb-2">
                 <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0">
@@ -411,6 +427,39 @@ const HomePage = () => {
                 </span>
                 Marketed across our social profiles
               </p>
+              <p className="flex items-center text-gray-600 mb-2">
+                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2.5"
+                    className="w-3 h-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span>
+                Distributed via Google Jobs recruitment network ASAP
+              </p>
+              <p className="flex items-center text-gray-600 mb-2">
+                <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2.5"
+                    className="w-3 h-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 6L9 17l-5-5"></path>
+                  </svg>
+                </span>
+                No account required
+              </p>
+
               <p className="flex items-center text-gray-600 mb-6">
                 <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0">
                   <svg
@@ -425,8 +474,9 @@ const HomePage = () => {
                     <path d="M20 6L9 17l-5-5"></path>
                   </svg>
                 </span>
-                Get viewed on the global stage
+                Support a Web3 initiative :)
               </p>
+
               <button className="flex items-center mt-auto text-white bg-[#ed194a]/40 border-0 py-2 px-4 w-full focus:outline-none hover:bg-[#ed194a] rounded">
                 Coming Soon
                 <svg
@@ -443,13 +493,18 @@ const HomePage = () => {
               </button>
             </div>
           </div>
-          <div className="max-w-xs ">
+          <div className="max-w-xs flex flex-col items-center">
             <img
               src={bankLocker}
               alt="prop"
               // srcSet={`${bankLockerSM} 300w, ${bankLocker} 768w`}
               className="h-40 sm:h-72 w-40 sm:w-72"
             />
+            <p className="text-center tracking-wider">
+              Whether you are a Web3 startup, or a "Web2" company in the process
+              of migration, tap in to the vast landscape of Web3 and let us help
+              you source your next talented sorcerers 🔮
+            </p>
           </div>
         </div>
       </section>
