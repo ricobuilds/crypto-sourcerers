30b46cbf83957d253e0109700d0c7793f73718d9
0xreeko <enric_trillo@outlook.com>
2022-03-04 11:12:10 +0000

    added more to roleCard

--- src/components/RoleCard.tsx
+++ src/components/RoleCard.tsx
@@ -1,5 +1,25 @@
 // tis for mapping over every available job.
 const RoleCard = () => {
-  return <div className="roleCard p-4 bg-[#ebebeb] max-w-md h-[350px]"></div>;
+  return (
+    <div className="roleCard relative flex justify-between items-center border-l-2 border-[#ed194a] p-4 bg-[#ebebeb] min-w-xl w-full h-[100px] rounded-md text-black">
+      <div className="absolute top-0 right-0 p-1 bg-rose-500 rounded-tr-md">
+        <p className="text-[#ebebeb] text-[11px]"> 4h ago</p>
+      </div>
+      <div className="left">
+        <h2 className="font-bold">Role title</h2>
+        <div className="coreDetails">
+          <p className="text-gray-600 text-sm">
+            {"Remote, Anywhere"} - {"50,000"} - {"Permanent"}
+          </p>
+        </div>
+      </div>
+      <div className="right">
+        <img src="" alt="brand-logo" className="h-12 w-auto" />
+        <button className="bg-transparent border border-[#ed194a] hover:bg-[#ed194a]">
+          Show More
+        </button>
+      </div>
+    </div>
+  );
 };
 export default RoleCard;
--- src/pages/Roles.tsx
+++ src/pages/Roles.tsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet-async";
+import RoleCard from "src/components/RoleCard";
 
 const Roles = () => {
   return (
@@ -8,12 +9,19 @@ const Roles = () => {
           Roles - Crypto Sourcerers - World's #1 Gateway for Web3 Jobs & Talent.
         </title>
       </Helmet>
-      <div className="">
-        <p>hye</p>
-        <p>hye</p>
-        <p>hye</p>
-        <p>hye</p>
-        <p>hye</p>
+      <div className="flex py-12 border-amber-500 border w-full">
+        <div className="w-1/4">filtering system</div>
+        <div className="grid gap-y-3 justify-items-center w-3/4">
+          <RoleCard />
+          <RoleCard />
+          <RoleCard />
+          <RoleCard />
+          <RoleCard />
+          <RoleCard />
+          <RoleCard />
+          <RoleCard />
+          <RoleCard />
+        </div>
       </div>
     </>
   );
