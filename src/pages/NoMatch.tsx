b5df41ada3fe71c5fd9c3322cec5a3709fda58bf
0xreeko <enric_trillo@outlook.com>
2022-03-18 15:25:11 +0000

    added Helmet to noMatch

--- src/pages/NoMatch.tsx
+++ src/pages/NoMatch.tsx
@@ -1,5 +1,27 @@
+import { Helmet } from "react-helmet-async";
+
 const NoMatch = () => {
+  
   return (
+    <>
+    <Helmet>
+    <title>404 ERROR - Web3Source</title>
+    <meta name="copyright" content="Web3Source, all rights reserved." />
+    <meta
+      name="description"
+      content="You have accessed a page that is not present in our component tree."
+    />
+    <link rel="canonical" href="https://web3source.co/" />
+    <meta
+      name="keywords"
+      content="web3 developer jobs, NFT developer jobs, blockchain developer jobs, metaverse developer jobs, crypto developer jobs, augmented reality developer jobs, virtual reality developer jobs, marketing jobs, community manager jobs, DAO developer jobs, DeFi developer jobs"
+    />
+    <meta name="author" content="0xreeko" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+    ></meta>
+  </Helmet>
     <div className="learning mx-auto my-24 py-20 max-w-lg flex flex-col items-center justify-center">
       <div className="">
         <h1 className="text-8xl px-auto w-full">
@@ -16,6 +38,7 @@ const NoMatch = () => {
       </div>
       <p className="text-4xl text-center">no match, sori, pliz :V</p>
     </div>
+    </>
   );
 };
 export default NoMatch;
