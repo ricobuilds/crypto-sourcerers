835308009a3191a99de4af894a9c6b93c5f9a11d
0xreeko <enric_trillo@outlook.com>
2022-03-18 12:57:38 +0000

    added Startup page

---
+++ src/pages/Startup.tsx
@@ -0,0 +1,7 @@
+const Startup = () => {
+  // display laoding state
+  // fetch data from DB -> query Startup table
+  // display data AND reset loading state
+  return <div className="startup">hey there this is AVAX.</div>;
+};
+export default Startup;
